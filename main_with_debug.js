3f9d91d7fdd13029f7fcb4a19ddb071e344817f1
bandrusko <benjamin.andrusko@gmail.com>
2025-06-09 20:15:39 -0500

    Add files via upload

---
+++ main_with_debug.js
@@ -0,0 +1,135 @@
+//Added function to initialize the script.
+//Added debugAjax to initialize the debug.
+function initialize() {
+  cities();
+  debugAjax();
+};
+
+/*Added a function which will properly plug in the array for its use. Put all
+of the functions under one function to create the table.
+*/
+function cities() {
+	var cityPop = [
+		{
+			city: 'Madison',
+			population: 233209
+		},
+		{
+			city: 'Milwaukee',
+			population: 594833
+		},
+		{
+			city: 'Green Bay',
+			population: 104057
+		},
+		{
+			city: 'Superior',
+			population: 27244
+		}
+	];
+	/*Creates the table and its elements so the array can be organized and shown
+	when the index is initialized.
+	*/
+	var table = document.createElement("table");
+	//
+	var headerRow = table.insertRow();
+
+	//Creates the header for the city column.
+	var cityHeader = document.createElement("th");
+	cityHeader.innerHTML = "City";
+	headerRow.appendChild(cityHeader);
+
+	//Creates the header for the population column.
+	var popHeader = document.createElement("th");
+	popHeader.innerHTML = "Population";
+	headerRow.appendChild(popHeader);
+
+	//Populate the table with city data rows.
+	cityPop.forEach(function (cityData) {
+		var row = table.insertRow();
+		var cityNameCell = row.insertCell();
+		cityNameCell.innerHTML = cityData.city;
+		var cityPopCell = row.insertCell();
+		cityPopCell.innerHTML = cityData.population;
+	});
+
+	//Put in this statement to print this within the index.
+	var mydiv = document.getElementById("mydiv");
+	mydiv.appendChild(table);
+
+	//Called the functions so they can be populate the third column.
+	addColumns(cityPop);
+	//Registered the event listener for the background color.
+	addEvents();
+};
+
+//Reorganized this so the function can be called.
+function addColumns(cityPopData) {
+	document.querySelectorAll('tr').forEach(function (row, i) {
+		console.log(i);
+		if (i === 0) {
+			row.insertAdjacentHTML('beforeend', '<th>City Size</th>');
+		} else {
+			var citySize;
+			//cityPopData is 0-indexed, but our loop starts from i=1 (after header), so use i-1
+			if (cityPopData[i - 1].population < 100000) {
+				citySize = 'Small';
+			} else if (cityPopData[i - 1].population < 500000) {
+				citySize = 'Medium';
+			} else {
+				citySize = 'Large';
+			}
+			// Add the new cell with city size data
+			row.insertAdjacentHTML('beforeend', '<td>' + citySize + '</td>');
+		};
+	});
+};
+//Reorganized this so this function cal also be called.
+function addEvents() {
+	/*Adjusted the function to work effectively. Swapped location of the function and
+	"mouseover" while also moving the parenthesis in the right place. Also swapped
+	"mouseover" with the declared variable 'color' since "mouseover" was not in this 
+	script.
+	*/
+	document.querySelector('table').addEventListener('mouseover', function () {
+		var randomColor = 'rgb(';
+		for (var i = 0; i < 3; i++) {
+			var randomNum = Math.round(Math.random() * 255);
+			randomColor += randomNum;
+			if (i < 2) {
+				randomColor += ',';
+			}
+		}
+		randomColor += ')';
+		// Change the background color of the table.
+		document.querySelector('table').style.backgroundColor = randomColor;
+	});
+	function clickme() {
+
+		alert('Hey, you clicked me!');
+	};
+	
+	//Registered the event listener for clicks.
+	document.querySelector("table").addEventListener("click", clickme)
+};
+
+//Added window onload statement to make the array run on the website.
+window.onload = initialize();
+
+//Removed # from mydiv.
+function debugCallback(response) {
+  document.getElementById("mydiv").insertAdjacentHTML('beforeend', 'GeoJSON data: ' + JSON.stringify(response))
+};
+
+//Turned string into JSON so the function could be read and printed. 
+//Created a second callback in order to read the file as a JSON. This
+//Also prints out the function on the HTML.
+function debugAjax() {
+    fetch("data/MegaCities.GeoJSON")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(debugCallback);
+};
+//Deleted lines 14-17 on the debug ajax code since the function can run more
+//fluently with the first statement.
